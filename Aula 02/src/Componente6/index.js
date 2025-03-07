ca1bec8e0865920e1d2dc3294eddfa118e2471dd
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-03-07 16:22:05 -0300

    Update index.js

--- Aula 02/src/Componente6/index.js
+++ Aula 02/src/Componente6/index.js
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native';
 import {viewStyle, textStyle} from './style'
 
 function Componente6(){  
-  let projetos = 'github.com/isaquesv';
+  let projetos = 'github.com/ruann-fernandess';
 
   return(
     <View style={viewStyle.espacamentoEsquerda}>
