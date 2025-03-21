6cecd04b8fa15676930e1a9aff7779cff3a656c3
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-03-21 17:47:43 -0300

    Delete calculo-do-imc directory

--- calculo-do-imc/App.js
+++
@@ -1,9 +0,0 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
-import Calculador from './components/pages/calculo-imc/Calculador'
-
-export default function App() {
-  return (
-    <Calculador/>
-  );
-}
-
