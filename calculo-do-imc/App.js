acfaf572380c9712741ea69984fd96edc3e496e8
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-03-21 17:47:22 -0300

    Aula 05 - Cálculo de IMC

---
+++ calculo-do-imc/App.js
@@ -0,0 +1,9 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import Calculador from './components/pages/calculo-imc/Calculador'
+
+export default function App() {
+  return (
+    <Calculador/>
+  );
+}
+
